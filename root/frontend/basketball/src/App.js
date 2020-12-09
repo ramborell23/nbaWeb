db5ff32214e3fc1aacdbab656be6a97fa8b0ece5
ramborell23 <jerelldavis@ac.c4q.nyc>
2020-12-08 22:55:20 -0500

    Update to Header menu

--- root/frontend/basketball/src/App.js
+++ root/frontend/basketball/src/App.js
@@ -10,28 +10,34 @@ import About from './Components/About.js';
 function App() {
   return (
     <Router>
-      
       <div className="App">
-      <header className="App-header">
-        <div>
-          <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            {/* <Link to="/dashboard">Dashboard</Link> */}
-          </li>
-          </ul>
-        </div>
+        <header className="App-header">
+          <div>
+            <ul
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                listStyleType: "none",
+                color: "white",
+                textDecoration: "none",
+              }}
+            >
+              <li style={{ color: "white", textDecoration: "none" }}>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>{/* <Link to="/dashboard">Dashboard</Link> */}</li>
+            </ul>
+          </div>
         </header>
-          {/* <img src={basket} className="App-logo" alt="logo" />
+        {/* <img src={basket} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p> */}
-        <hr/>
+        <hr />
         {/* <FormPost /> */}
         <Switch>
           <Route exact path="/">
@@ -45,7 +51,7 @@ function App() {
           </Route> */}
         </Switch>
       </div>
-      </Router>
+    </Router>
   );
 }
 
